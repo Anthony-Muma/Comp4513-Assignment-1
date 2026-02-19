a81af8827fcdab8bacc1cdf6999c1a9d99e7ff44
Anthony Muma <amuma917@mtroyal.ca>
2026-02-18 18:46:09 -0700

    finsihed server.js

--- server.js
+++ server.js
@@ -1,45 +1,37 @@
 const express = require("express");
-
-// create express app
 const app = express();
 
-// set up route handling 
+// route handling 
 const artistRouter = require('./scripts/artists-router.js');
-const { handleAllArtist, handleArtistRef, handleArtistRefAverage } = artistRouter;
-handleAllArtist(app);
-handleArtistRef(app);
-handleArtistRefAverage(app);
+artistRouter.handleAllArtist(app);
+artistRouter.handleArtistRef(app);
+artistRouter.handleArtistRefAverage(app);
 
 const genreRouter = require('./scripts/genre-router.js');
-const { handleAllGenres } = genreRouter;
-handleAllGenres(app);
+genreRouter.handleAllGenres(app);
 
 const playlistRouter = require('./scripts/playlists-router.js');
-const { handlePlaylistsRef } = playlistRouter;
-handlePlaylistsRef(app);
+playlistRouter.handlePlaylistsRef(app);
 
 const songsRouter = require('./scripts/songs-router.js');
-const { handleAllSongs, handleAllSongsSort, handleSongsRef, handleSongsSearchBegin, handleSongsSearchAny, handleSongsSearchYear, handleSongsArtistRef, handleSongsGenreRef } = songsRouter;
-handleAllSongs(app);
-handleAllSongsSort(app);
-handleSongsRef(app);
-handleSongsSearchBegin(app);
-handleSongsSearchAny(app);
-handleSongsSearchYear(app);
-handleSongsArtistRef(app);
-handleSongsGenreRef(app);
+songsRouter.handleAllSongs(app);
+songsRouter.handleAllSongsSort(app);
+songsRouter.handleSongsRef(app);
+songsRouter.handleSongsSearchBegin(app);
+songsRouter.handleSongsSearchAny(app);
+songsRouter.handleSongsSearchYear(app);
+songsRouter.handleSongsArtistRef(app);
+songsRouter.handleSongsGenreRef(app);
 
 const moodRouter = require('./scripts/mood-router.js');
-const { handleDancingMoodRef, handleHappyMoodRef, handleCoffeeMoodRef, handleStudyingMoodRef} = moodRouter;
-handleDancingMoodRef(app);
-handleHappyMoodRef(app);
-handleCoffeeMoodRef(app);
-handleStudyingMoodRef(app);
+moodRouter.handleDancingMoodRef(app);
+moodRouter.handleHappyMoodRef(app);
+moodRouter.handleCoffeeMoodRef(app);
+moodRouter.handleStudyingMoodRef(app);
 
 
-
-// use express to listen to port 
-let port = process.env.PORT; 
+// listen to port 
+const port = process.env.PORT || 3000; 
 app.listen(port, () => { 
     console.log("Server running at port = " + port); 
 });

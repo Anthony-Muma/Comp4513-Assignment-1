e86801fc37ee76aeef5148538750eda6ad59aa1e
Anthony Muma <amuma917@mtroyal.ca>
2026-02-18 18:11:52 -0700

    finished genre route

--- scripts/genre-router.js
+++ scripts/genre-router.js
@@ -1,18 +1,18 @@
 const dataProvider = require("./data-provider.js");
-const { dbAll, dbGet } = dataProvider;
+const { dbAll } = dataProvider;
 
-const genreSql = `
-    SELECT
-       genre_name
-    FROM
-        genres
-    ; 
-`;
+const GENRE_SQL = `SELECT genre_name FROM genres`;
 
+/**
+ * ***./api/genres***
+ * 
+ * Returns all the genres
+ * @param {import('express').Application} app - Express app
+ */
 function handleAllGenres(app) {
     app.get('/api/genres', async (req, resp) => {
         try {
-            const rows = await dbAll(genreSql);
+            const rows = await dbAll(GENRE_SQL);
             resp.json(rows);
         } catch (error) {
             console.error(error.message);
